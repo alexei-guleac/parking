c981e6ee319b7b6921fef86be9bbe1d2f01f2e99
aleks.g <aleks7900@gmail.com>
2020-02-28 22:02:20 +0200

    setup backend for local develop

--- Backend/src/main/java/com/isd/parking/config/CORSConfig.java
+++ Backend/src/main/java/com/isd/parking/config/CORSConfig.java
@@ -6,19 +6,23 @@
 package com.isd.parking.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 /**
- * Enable CORS access to resources from the frontend application,
+ * Enable CORS access to resources from the frontend,
  * connection point between frontend SPA and backend API
  * specifies in a flexible way what kind of cross domain requests are authorized
  */
 @Configuration
 @Slf4j
 public class CORSConfig implements WebMvcConfigurer {
+
+    @Value("${front.url}")
+    private String frontUrl;
 
     /**
      * Method specifies CORS mapping - which methods are allowed from allowed origins (frontend Angular SPA)
@@ -28,8 +32,10 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
+        log.info("Call from front");
+
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOrigins("*");  //TODO: change the URL for the prod URL when we deploy
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                .allowedOrigins(frontUrl);  //TODO: change the URL for the prod URL when we deploy
     }
 }
